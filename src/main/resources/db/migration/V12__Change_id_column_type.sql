56ee9c81a1d8e6be559bc81b8f0bf8c48f05eef5
xiaotxiaox <630356213@qq.com>
2019-12-03 10:21:25 +0800

    修复数据库切换后遗症

--- src/main/resources/db/migration/V12__Change_id_column_type.sql
+++ src/main/resources/db/migration/V12__Change_id_column_type.sql
@@ -1,1 +1,3 @@
 alter table comment alter column id type BIGINT using id::BIGINT;
+alter table comment alter column commentator type bigint using commentator::bigint;
+
--- src/main/resources/db/migration/V9__Add_comment_count_to_comment_table.sql
+++ src/main/resources/db/migration/V9__Add_comment_count_to_comment_table.sql
@@ -1,1 +1,2 @@
 alter table comment add comment_count int default 0;
+alter table question alter column creator type bigint using creator::bigint;
